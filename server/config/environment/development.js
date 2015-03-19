4f903d793e68fae46d61731777f2971e9b2abb67
Paget Fink <pagetfink@gmail.com>
2015-03-18 21:59:41 -0400

    use dev as prod

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,11 +1,36 @@
 'use strict';
 
-// Development specific configuration
-// ==================================
+//// Development specific configuration
+//// ==================================
+//module.exports = {
+//  // MongoDB connection options
+//  mongo: {
+//    uri: 'mongodb://localhost/transistor-dev'
+//  },
+//
+//  seedDB: true
+//};
+//
+//
+//// Production specific configuration
+//// =================================
 module.exports = {
+  // Server IP
+  ip:       process.env.OPENSHIFT_NODEJS_IP ||
+  process.env.IP ||
+  undefined,
+
+  // Server port
+  port:     process.env.OPENSHIFT_NODEJS_PORT ||
+  process.env.PORT ||
+  8080,
+
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/transistor-dev'
+    uri:    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+    'mongodb://localhost/transistor'
   },
 
   seedDB: true
